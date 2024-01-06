678be3a9431bf7eb3839ffdb714d6bd75aabd5be
Nikhil sharma <134368528+Nikhilsharma-tech@users.noreply.github.com>
2024-01-06 18:51:46 +0000

    conversion and operation done

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,34 +1,79 @@
-let number = ""
+// let number = ""
 
-console.log(typeof (number))
+// console.log(typeof (number))
 
-let isBoolean = Boolean(number)
-console.log(isBoolean)
+// let isBoolean = Boolean(number)
+// console.log(isBoolean)
 
-let myName = "33"
+// let myName = "33"
 
-let isNumber = Number(myName)
-console.log(isNumber)
+// let isNumber = Number(myName)
+// console.log(isNumber)
 
-let isString = String(myName)
-console.log(isString)
+// let isString = String(myName)
+// console.log(isString)
 
-console.log(typeof (myName))
+// console.log(typeof (myName))
 
-// "33" => 33
-// "33abc" => NaN (Not a number)
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN (Not a number)
+// // true => 1; false => 0
 
-let isloggedIn = "33"
+// let isloggedIn = "33"
 
-let isitNumber = Number(isloggedIn)
-console.log(isitNumber)
+// let isitNumber = Number(isloggedIn)
+// console.log(isitNumber)
 
-// 1 => true; 0 => false
-// "" => false
-// "Nikhil" => true
+// // (for boolean)
+// // 1 => true; 0 => false
+// // "" => false
+// // "Nikhil" => true
 
-let Nameis ="Nikhil"
+// let Nameis ="Nikhil"
 
-let booleanNameis = Boolean(Nameis)
-console.log(booleanNameis)
+// let booleanNameis = Boolean(Nameis)
+// console.log(booleanNameis)
+
+
+// *******************************OPERATIONS**********************************
+
+let number = 3
+let negValue = -number
+console.log(negValue)
+
+// console.log(3 + 3)
+// console.log(3 - 2)
+// console.log(3 * 4)
+// console.log(2 ** 3)
+// console.log(2 / 1)
+// console.log(3 % 2)
+
+let str1 = "Hello "
+let str2 = "Nikhil"
+
+let str3 = str1 + str2
+console.log(str3)
+console.log(str1 + str2)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
+++gamecounter
+console.log(gamecounter)
+
+let plancounter = 100
+plancounter--
+console.log(plancounter)
